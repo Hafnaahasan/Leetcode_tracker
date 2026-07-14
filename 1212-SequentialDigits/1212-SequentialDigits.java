b60d8fcb0ecccadd43057375761437ee556da659
Hafnaahasan <hafnaa123456@gmail.com>
2026-07-14 14:14:22 +0530

    Create 1212-SequentialDigits

---
+++ 1212-SequentialDigits/1212-SequentialDigits.java
@@ -0,0 +1,21 @@
+// Last updated: 7/14/2026, 2:14:21 PM
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> ans = new ArrayList<>();
+
+        String s ="123456789";
+        String l = String.valueOf(low);
+        String h = String.valueOf(high);
+
+        for(int len=l.length();len<=h.length();len++){
+            for(int start =0;start<=9-len;start++){
+                int num = Integer.parseInt(s.substring(start,start+len));
+                if(num >= low&&num<= high){
+                    ans.add(num);
+                }
+            }
+        }
+        return ans;
+
+    }
+}

9eace55ac7266f64222073d83c87163b6e2c4c8e
Hafnaahasan <hafnaa123456@gmail.com>
2026-07-14 14:14:14 +0530

    Create 2816-LexicographicallySmallestPalindrome

---
+++ 2816-LexicographicallySmallestPalindrome/2816-LexicographicallySmallestPalindrome.java
@@ -0,0 +1,10 @@
+// Last updated: 7/14/2026, 2:14:12 PM
+class Solution {
+    public String makeSmallestPalindrome(String s) {
+        int n = s.length();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<n;i++)
+        sb.append(Character.toString(Math.min(s.charAt(i),s.charAt(n-i-1))));
+        return sb.toString();
+    }
+}

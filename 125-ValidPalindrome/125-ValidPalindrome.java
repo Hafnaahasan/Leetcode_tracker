6761293190a3ebecb545bbd315df75eec30d0a33
Hafnaahasan <hafnaa123456@gmail.com>
2026-07-14 14:15:33 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -0,0 +1,28 @@
+// Last updated: 7/14/2026, 2:15:31 PM
+class Solution {
+    public boolean isPalin(String s) {
+        int i=0;
+        int j=s.length()-1;
+
+        while(i<j){
+            if(s.charAt(i) !=s.charAt(j)) return false;
+
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public boolean isPalindrome(String s){
+        s=s.toLowerCase();
+        String ans="";
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            int ascii=(int)ch;
+            if((ascii>=97&& ascii <=122)||(ascii >=48&& ascii<=57)){
+                ans += ch;
+            }
+        }
+        System.out.println(ans);
+        return isPalin(ans);
+    }
+}

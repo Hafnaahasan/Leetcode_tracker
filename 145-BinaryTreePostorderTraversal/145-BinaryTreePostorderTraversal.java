5e53252daae3f80d412abeea05df1956cb5308c5
Hafnaahasan <hafnaa123456@gmail.com>
2026-07-14 14:15:29 +0530

    Create 145-BinaryTreePostorderTraversal

---
+++ 145-BinaryTreePostorderTraversal/145-BinaryTreePostorderTraversal.java
@@ -0,0 +1,29 @@
+// Last updated: 7/14/2026, 2:15:27 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> result(TreeNode root,List<Integer>list) {
+        if(root==null) return list;
+        result(root.left,list);
+        result(root.right,list);
+        list.add(root.val);
+        return list;
+    }
+       public List<Integer> postorderTraversal(TreeNode root){
+        ArrayList<Integer>  list = new ArrayList<Integer>();
+        return result(root,list);
+       }
+}
